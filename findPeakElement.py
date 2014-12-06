a60cac2e52c8db8c7d440b973004caceed4c4796
isaiah_qian <qianlongzju@gmail.com>
2014-12-06 22:50:13 +0800

    solution for find peak element  NO.162

---
+++ findPeakElement.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def findPeakElement(self, num):
+    	return self.findPeakElementInterval(num, 0, len(num)-1)
+
+    def findPeakElementInterval(self, num, start, end):
+    	if start == end:
+    		return start
+    	mid = start + (end - start)/2
+    	if (mid == 0 or num[mid] > num[mid-1]) and (mid+1 > end or num[mid] > num[mid+1]):
+    		return mid
+    	elif (mid == 0 or num[mid] > num[mid-1]) and (mid+1 > end or num[mid] < num[mid+1]):
+    		return self.findPeakElementInterval(num, mid+1, end)
+    	else:
+    		return self.findPeakElementInterval(num, start, end-1)
+
+if __name__ == '__main__':
+	s = Solution()
+	num = [1, 2]
+	print s.findPeakElement(num)
