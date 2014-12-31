e7e351931b6b2a8a765d9fafea71a72e4df72ddb
Long Qian <qianlongzju@gmail.com>
2014-12-31 10:42:18 +0800

    solution for NO.172

---
+++ factorialTrailingZeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+    	int a = n/5;
+        int count = a;
+        while (a > 0) {
+        	count += a/5;
+        	a /= 5;
+        }
+        return count;
+    }
+};
---
+++ factorialTrailingZeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @return an integer
+    def trailingZeroes(self, n):
+        a = 5
+        count = 0
+        while a <= n:
+        	count += n/a
+        	a *= 5
+        return count
