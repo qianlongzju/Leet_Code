57ecfe32edca8e2d8704730a1fa4a1e6556068c6
Long Qian <qianlongzju@gmail.com>
2015-04-05 15:55:46 +0800

    refactor maximumDepthofBinaryTree

---
+++ Algorithms/maximumDepthofBinaryTree.cpp
@@ -0,0 +1,9 @@
+#include "leetcode.h"
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL)
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
