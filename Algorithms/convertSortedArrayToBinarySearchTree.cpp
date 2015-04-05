512815ce17ed4033d85b17f1ffcfe978391cf2b2
Long Qian <qianlongzju@gmail.com>
2015-04-05 18:04:34 +0800

    refactor convertSortedArrayToBinarySearchTree

---
+++ Algorithms/convertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,15 @@
+#include "leetcode.h"
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        return sortedArrayToBST(num, 0, num.size()-1);
+    }
+    TreeNode *sortedArrayToBST(vector<int> &num, int start, int end) {
+        if (start > end) return NULL;
+        int middle = start + (end-start)/2;
+        TreeNode* root = new TreeNode(num[middle]);
+        root->left = sortedArrayToBST(num, start, middle-1);
+        root->right = sortedArrayToBST(num, middle+1, end);
+        return root;
+    }
+};
--- Algorithms/convertSortedArrayToBinarySearchTree.java
+++ Algorithms/convertSortedArrayToBinarySearchTree.java
@@ -10,15 +10,14 @@ import java.util.*;
  */
 public class Solution {
     public TreeNode sortedArrayToBST(int[] num) {
-        return sortedSubArrayToBST(num, 0, num.length-1);
+        return sortedArrayToBST(num, 0, num.length-1);
     }
-    TreeNode sortedSubArrayToBST(int[] num, int start, int end) {
-        if (start > end) 
-            return null;
+    private TreeNode sortedArrayToBST(int[] num, int start, int end) {
+        if (start > end) return null;
         int middle = start + (end-start)/2;
         TreeNode root = new TreeNode(num[middle]);
-        root.left = sortedSubArrayToBST(num, start, middle-1);
-        root.right = sortedSubArrayToBST(num, middle+1, end);
+        root.left = sortedArrayToBST(num, start, middle-1);
+        root.right = sortedArrayToBST(num, middle+1, end);
         return root;
     }
 }
