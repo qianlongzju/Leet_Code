998b8586872738717951249b37f5b3bd4863c7fe
Long Qian <qianlongzju@gmail.com>
2015-04-28 15:39:03 +0800

    refactor nQueens

--- Algorithms/nQueens.java
+++ Algorithms/nQueens.java
@@ -1,35 +1,29 @@
-import java.util.*;
 public class Solution {
-    public ArrayList<String[]> solveNQueens(int n) {
+    public List<String[]> solveNQueens(int n) {
         int[] num = new int[n];
         for (int i=0; i < n; i++) {
             num[i] = 0;
         }
-        ArrayList<String[]> result = new ArrayList<String[]>();
-        solveNQueens(n, num, result);
+        List<String[]> result = new ArrayList<String[]>();
+        solveNQueens(n, num, result, 0);
         return result;
     }
-    void solveNQueens(int n, int[] num, ArrayList<String[]> result) {
-        for (int i=0; i < n; ++i) {
-            if (num[i] != 0)
-                continue;
-            for (int j=1; j <= n; ++j) {
-                num[i] = j;
-                if (isValid(num, i))
-                    solveNQueens(n, num, result);
-            }
-            num[i] = 0;
+    void solveNQueens(int n, int[] num, List<String[]> result, int level) {
+        if (level == n) {
+            result.add(getBoard(num));
             return;
         }
-        result.add(getBoard(num));
+        for (int j=1; j <= n; ++j) {
+            num[level] = j;
+            if (isValid(num, level))
+                solveNQueens(n, num, result, level+1);
+        }
     }
     boolean isValid(int[] num, int index) {
         for (int i=0; i < index; ++i) {
             if (num[i] == num[index])
                 return false;
-            if ((num[i] - num[index]) == (i - index))
-                return false;
-            if ((num[i] - num[index]) == (index - i))
+            if (Math.abs(num[i] - num[index]) == (index - i))
                 return false;
         }
         return true;
