897ac443f7d8b707da2baf961947a944d67b7093
Long Qian <qianlongzju@gmail.com>
2015-04-28 16:55:48 +0800

    refactor sudokuSolver

--- sudokuSolver.java
+++ sudokuSolver.java
@@ -1,5 +1,20 @@
-import java.util.*;
 public class Solution {
+    public boolean solveSudoku(char[][] board) {
+        for (int i=0; i < 9; ++i) {
+            for (int j=0; j < 9; ++j) {
+                if (board[i][j] == '.') {
+                    for (int k=1; k <= 9; ++k) {
+                        board[i][j] = (char)((int)'0' + k);
+                        if (isValid(board, i, j) && solveSudoku(board))
+                            return true;
+                    }
+                    board[i][j] = '.';
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     boolean isValid(char[][] board, int x, int y) {
         for (int i=0; i < 9; i++)
             if (i != x && board[i][y] == board[x][y])
@@ -11,24 +26,6 @@ public class Solution {
             for (int j=(y/3)*3; j < ((y/3)+1)*3; j++)
                 if (i != x && j != y && board[i][j] == board[x][y])
                     return false;
-        return true;
-    }
-    public boolean solveSudoku(char[][] board) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        for (int i=0; i < 9; ++i) {
-            for (int j=0; j < 9; ++j) {
-                if (board[i][j] == '.') {
-                    for (int k=1; k <= 9; ++k) {
-                        board[i][j] = (char)((int)'0' + k);
-                        if (isValid(board, i, j) && solveSudoku(board))
-                            return true;
-                        //board[i][j] = '.';
-                    }
-                    board[i][j] = '.';
-                    return false;
-                }
-            }
-        }
         return true;
     }
 }
---
+++ sudokuSolver.py
@@ -0,0 +1,31 @@
+class Solution:
+    # @param board, a 9x9 2D array
+    # Solve the Sudoku by modifying the input board in-place.
+    # Do not return any value.
+    def solveSudoku(self, board):
+        self.solveSudokuHelper(board)
+
+    def solveSudokuHelper(self, board):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    continue
+                for k in range(9):
+                    board[i][j] = str(k + 1)
+                    if self.isValid(board, i, j) and self.solveSudokuHelper(board):
+                        return True
+                board[i][j] = '.'
+                return False
+        return True
+
+    def isValid(self, board, x, y):
+        for i in range(9):
+            if i != x and board[i][y] == board[x][y]:
+                return False
+            if i != y and board[x][i] == board[x][y]:
+                return False
+        for i in range((x/3)*3, (x/3+1)*3):
+            for j in range((y/3)*3, (y/3+1)*3):
+                if i != x and j != y and board[i][j] == board[x][y]:
+                    return False
+        return True
