bb4d94631e86ddc5206176f739715872aea634d5
Long Qian <qianlongzju@gmail.com>
2015-04-28 14:38:04 +0800

    refactor triangle

--- Algorithms/triangle.cpp
+++ Algorithms/triangle.cpp
@@ -4,12 +4,7 @@ public:
     int minimumTotal(vector<vector<int> > &triangle) {
         for (int i=triangle.size()-2; i >= 0; i--) {
             for (int j=0; j < triangle[i].size(); j++) {
-                if (triangle[i+1][j] < triangle[i+1][j+1]) {
-                    triangle[i][j] += triangle[i+1][j];
-                }
-                else {
-                    triangle[i][j] += triangle[i+1][j+1];
-                }
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
             }
         }
         return triangle[0][0];
