9ddc324babb563b4d8edcbe3c6cab896b5a9beff
Long Qian <qianlongzju@gmail.com>
2015-05-22 13:30:50 +0800

    add blog post for reverseLinkedListII.py

--- Algorithms/reverseLinkedListII.py
+++ Algorithms/reverseLinkedListII.py
@@ -1,3 +1,5 @@
+# http://jessicasco.github.io/blog/2015/05/22/python-swap-caution/
+from leetcode import *
 class Solution:
     # @param {ListNode} head
     # @param {integer} m
@@ -13,9 +15,18 @@ class Solution:
             if i < m:
                 ins, p = ins.next, p.next
             else:
+                #temp = p.next
+                #p.next = temp.next
+                #temp.next = ins.next
+                #ins.next = temp
+
                 #p.next, p.next.next, ins.next =  p.next.next, ins.next, p.next
-                temp = p.next
-                p.next = temp.next
-                temp.next = ins.next
-                ins.next = temp
+                p.next.next, p.next,  ins.next =  ins.next, p.next.next, p.next
         return headptr.next
+
+if __name__ == '__main__':
+    S = Solution()
+    #head = ListNode(3)
+    #head.next = ListNode(5)
+    head, head.next = ListNode(3), ListNode(5)
+    S.reverseBetween(head, 1, 2)
