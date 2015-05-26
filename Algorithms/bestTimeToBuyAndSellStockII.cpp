a044ded0bad914baa4289ea6f6bee3190350dd33
Long Qian <qianlongzju@gmail.com>
2015-05-26 16:53:55 +0800

    refactor bestTimeToBuyAndSellStockII

--- Algorithms/bestTimeToBuyAndSellStockII.cpp
+++ Algorithms/bestTimeToBuyAndSellStockII.cpp
@@ -2,15 +2,10 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
+        if (prices.size() <= 1) return 0;
         int profit = 0;
-        if (prices.size() <= 1) {
-            return 0;
-        }
-        for (int i=1; i < prices.size(); i++) {
-            if (prices[i] > prices[i-1]) {
-                profit += prices[i] - prices[i-1];
-            } 
-        }
+        for (int i=1; i < prices.size(); i++)
+            profit += max(0, prices[i] - prices[i-1]);
         return profit;
     }
 };
---
+++ Algorithms/bestTimeToBuyAndSellStockII.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param {integer[]} prices
+    # @return {integer}
+    def maxProfit(self, prices):
+        if len(prices) <= 1: return 0
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(0, prices[i] - prices[i-1])
+        return profit
