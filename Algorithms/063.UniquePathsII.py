23e65e91ae2591259a8e3ac998611e0681ce096d
钱龙 <long.qian@ele.me>
2018-07-22 10:33:57 +0800

    063 py

--- Algorithms/063.UniquePathsII.py
+++ Algorithms/063.UniquePathsII.py
@@ -1,9 +1,12 @@
-class Solution:
-    # @param obstacleGrid, a list of lists of integers
-    # @return an integer
+class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
         m = len(obstacleGrid)
-        if not n: return 0
+        if m == 0:
+            return 0
         n = len(obstacleGrid[0])
         paths = [[1 for i in range(n)] for i in range(m)]
         for i in range(m):
@@ -11,9 +14,9 @@ class Solution:
                 if obstacleGrid[i][j]:
                     paths[i][j] = 0
                 elif i and j:
-                    paths[i][j] = paths[i-1][j] + paths[i][j-1]
+                    paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
                 elif i:
-                    paths[i][j] = paths[i-1][j]
+                    paths[i][j] = paths[i - 1][j]
                 elif j:
-                    paths[i][j] = paths[i][j-1]
-        return paths[m-1][n-1]
+                    paths[i][j] = paths[i][j - 1]
+        return paths[m - 1][n - 1]
