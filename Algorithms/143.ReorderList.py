1e780e2c5968fb55e2cee662a3baefd8e168e5e9
钱龙 <long.qian@ele.me>
2018-07-22 14:02:33 +0800

    143 py

--- Algorithms/143.ReorderList.py
+++ Algorithms/143.ReorderList.py
@@ -1,11 +1,18 @@
-class Solution:
-    # @param head, a ListNode
-    # @return a ListNode
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
     def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
         p = self.split(head)
         p = self.reverse(p)
         head = self.merge(head, p)
-        return head
 
     def split(self, head):
         if not head:
