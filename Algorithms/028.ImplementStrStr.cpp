4d86b83ce033fea06715293d4e1c45f7cc263a57
钱龙 <long.qian@ele.me>
2018-08-03 20:54:34 +0800

    028 cpp

--- Algorithms/028.ImplementStrStr.cpp
+++ Algorithms/028.ImplementStrStr.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int strStr(char *haystack, char *needle) {
-        if (haystack == NULL || needle == NULL) {
-            return -1;
+    int strStr(string haystack, string needle) {
+        if (needle == "") {
+            return 0;
         }
-        int lenstr = strlen(haystack);
-        int lensubstr = strlen(needle);
+        int lenstr = haystack.size();
+        int lensubstr = needle.size();
         if (lenstr < lensubstr) {
             return -1;
         }
