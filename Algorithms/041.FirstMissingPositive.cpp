778652f530f0b12797e01a8878d0c9be61340571
钱龙 <long.qian@ele.me>
2018-08-04 10:58:07 +0800

    041

---
+++ Algorithms/041.FirstMissingPositive.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i=0; i < n; i++) {
+            while (nums[i] <= n  && nums[i] > 0 && nums[i] != i+1) {
+                int temp = nums[nums[i]-1];
+                if (temp == nums[i]) {
+                    break;
+                }
+                nums[nums[i]-1] = nums[i];
+                nums[i] = temp;
+            }
+        }        
+        for (int i=0; i < n; i++) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
