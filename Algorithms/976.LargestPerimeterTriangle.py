d62d1acaf907027c6dcb43e1b841bfe8db791c49
酒萧 <long.qianl@alibaba-inc.com>
2019-03-18 23:31:26 +0800

    976

---
+++ Algorithms/976.LargestPerimeterTriangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        A.sort(reverse=True)
+        for i in range(2, len(A)):
+            if A[i]+A[i-1] > A[i-2]:
+                return A[i]+A[i-1]+A[i-2]
+        return 0
